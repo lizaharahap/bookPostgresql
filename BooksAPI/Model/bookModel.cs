c34cab94129e1655dd00f087db0d86e20a4c3c93
lizaharahap <rizky.nharahap@bukitmakmur.com>
2021-06-15 21:38:09 +0700

    Add project files.

---
+++ BooksAPI/Model/bookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksAPI.Model
+{
+    public class bookModel
+    {
+        [Key]
+        public int book_id { get; set; }
+        public string book_name{ get; set; }
+        public string book_author { get; set; }
+        public string book_isbn { get; set; }
+    }
+}
